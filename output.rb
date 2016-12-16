9dffc8f87397da06d20bee9527224d365228c6ac
Kathy Rodante <kathy.rodante@gmail.com>
2016-12-16 15:59:56 -0800

    add logic for given numbers greater than the csv count and odd numbers

--- output.rb
+++ output.rb
@@ -8,17 +8,49 @@ class Output
     questions = []
     hash_csv = QUESTIONS_CSV.map {|row| row.to_hash }
 
+    duplicate_csv(number, hash_csv, 1)
+
     questions_per_strand = number/2.to_i
-    prng = Random.new
-    strand_id = rand(1..2)
 
     strand_1 = hash_csv.find_all { |x| x[:strand_id] == "1" }
-    questions << strand_1.sample(questions_per_strand).map{ |x| x[:question_id] }
-
     strand_2 = hash_csv.find_all { |x| x[:strand_id] == "2" }
-    questions << strand_2.sample(questions_per_strand).map{ |x| x[:question_id] }
+
+    if number.odd?
+      questions = add_one_for_odd(questions, questions_per_strand, strand_1, strand_2)
+    else
+      questions << add_strand(questions_per_strand, strand_1)
+      questions << add_strand(questions_per_strand, strand_2)
+    end
 
     puts questions.flatten.inspect
+  end
+
+  def self.add_strand(questions_per_strand, strand, extra = 0)
+    strand.sample(questions_per_strand + extra).map { |x| x[:question_id] }
+  end
+
+  def self.add_one_for_odd(questions, questions_per_strand, strand_1, strand_2)
+    prng = Random.new
+    strand_id = rand(1..2)
+    if strand_id == 1
+      questions << add_strand(questions_per_strand, strand_1, 1)
+      questions << add_strand(questions_per_strand, strand_2)
+    else
+      questions << add_strand(questions_per_strand, strand_1)
+      questions << add_strand(questions_per_strand, strand_2, 1)
+    end
+    questions
+  end
+
+  def self.duplicate_csv(number, hash_csv, row)
+    if number >= hash_csv.count
+      new_hash_csv = hash_csv
+      new_hash_csv << hash_csv[row]
+      new_hash_csv
+      duplicate_csv(number, new_hash_csv, row + 1)
+    else
+      return new_hash_csv
+    end
   end
 
 end
